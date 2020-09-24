6012a1f76e44b151ab750793b49e99fe3ed34413
SeonghyeonL <kate1301@naver.com>
2020-09-25 00:54:01 +0900

    one time done - except repeating

--- plzplzplz/dsaplz1.c
+++ plzplzplz/dsaplz1.c
@@ -19,7 +19,7 @@ int main(void)
 		}
 		else
 		{
-			printf("length: %d\n", LoS); break;
+			printf("input length: %d\n", LoS); break;
 		}
 	}
 	insq = (int*)malloc(sizeof(int) * LoS);
@@ -38,6 +38,7 @@ int main(void)
 	{
 		printf("%d ", subseq[i + 1]);
 	}
+	printf("\n");
 	// subseq의 크기 계산해서 subseq를 for문으로 출력
 	free(insq); free(subseq);
 	return 0;
@@ -47,7 +48,7 @@ int main(void)
 MaxSubsequenceSum(int A[], int N)
 {
 	int ThisSum, MaxSum, j; int Amax, Anow;
-	ThisSum = MaxSum = 0; Amax = Anow = 0;
+	ThisSum = MaxSum = 0; Amax = -9999; Anow = 0;
 	for (j = 0;j < N;j++)
 	{
 		ThisSum += A[j];
@@ -56,49 +57,58 @@ MaxSubsequenceSum(int A[], int N)
 
 		Anow = A[j];
 		if (Anow > Amax)    Amax = Anow;
+		// Amax is max num for x<0 only sequence
 	}
-	if (MaxSum == 0 && Amax == 0)
-	{
-		ThisSum = 0;
-		for (j = 0;j < N;j++)
-		{
-			ThisSum = A[j];
-			if (ThisSum < MaxSum)    MaxSum = ThisSum;
-		}
-	}
+	if (MaxSum == 0 && Amax < 0)    MaxSum = Amax;
 	return MaxSum;
 }
 
 // edit - return 'subseq'
 // 조건: max subseq는 각 원소의 합이 최대가 되는 subseq 중
 //       구성 원소의 개수가 최소인 subseq라고 정의
+// MaxSum이 0인 경우는 0과 음수로만 구성된 경우 (= 0 1개)
+// MaxSum이 음수인 경우는 음수로만 구성된 경우 (= 최댓값인 1개)
+// MaxSum이 양수인 경우는 나머지
 MaxSubSeqSum(int A[], int N, int MaxSum)
 {
 	int thissum, maxsum, findsum;
 	int j, k, i, startnum, endnum;
-	thissum = maxsum = startnum = endnum = 0;
-	for (j = 0;j < N;j++)
+	int* SubSeq;
+	if (MaxSum > 0)
 	{
-		thissum += A[j];
-		if (thissum > maxsum)
+		thissum = maxsum = startnum = endnum = 0;
+		for (j = 0;j < N;j++)
 		{
-			maxsum = thissum;
-			if (A[j] != 0 && maxsum == MaxSum)    endnum = j;
+			thissum += A[j];
+			if (thissum > maxsum)
+			{
+				maxsum = thissum;
+				if (A[j] != 0 && maxsum == MaxSum)    endnum = j;
+			}
+			else if (thissum < 0)    thissum = 0;
 		}
-		else if (thissum < 0)    thissum = 0;
+		findsum = MaxSum;
+		for (k = endnum;k >= 0;k--)
+		{
+			findsum -= A[k];
+			if (A[k] != 0 && findsum == 0)    startnum = k;
+		}
+		int SubSeqLen = endnum - startnum + 1;
+		SubSeq = (int*)malloc(sizeof(int) * (SubSeqLen + 1));
+		for (i = 1;i <= SubSeqLen;i++)
+		{
+			SubSeq[i] = A[i + startnum - 1];
+		}
+		SubSeq[0] = SubSeqLen;
+		return SubSeq;
 	}
-	findsum = MaxSum;
-	for (k = endnum;k >= 0;k--)
+	else if (MaxSum <= 0) // MaxSum==0 && MaxSum<0
 	{
-		findsum -= A[k];
-		if (A[k] != 0 && findsum == 0)    startnum = k;
+		int SubSeqLen = 1; // "MaxSum"
+		SubSeq = (int*)malloc(sizeof(int) * (SubSeqLen + 1));
+		SubSeq[0] = SubSeqLen; SubSeq[1] = MaxSum;
+		return SubSeq;
 	}
-	int* SubSeq; int SubSeqLen = endnum - startnum + 1;
-	SubSeq = (int*)malloc(sizeof(int) * (SubSeqLen+1));
-	for (i = 1;i <= SubSeqLen;i++)
-	{
-		SubSeq[i] = A[i + startnum - 1];
-	}
-	SubSeq[0] = SubSeqLen;
-	return SubSeq;
 }
+
+// 문제점: 0과 음수로만 구성된 수열은 SubSeq가 음수로만 나옴
