46884e4392e99557207561a5bcfa2c4165789776
SeonghyeonL <kate1301@naver.com>
2020-09-26 01:41:58 +0900

    not yet

--- plzplzplz/dsaplz1.c
+++ plzplzplz/dsaplz1.c
@@ -1,45 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h> // for malloc, free
-#define _CRT_SECURE_NO_WARNINGS // for scanf error
+#define _CRT_SECURE_NO_WARNINGS // prevent scanf error
 
 int main(void)
 {
-	// 총 몇 개 배열 받는지 입력 부분 필요
-	int* insq; int LoS;
+	// *** 총 몇 개 배열 받는지 입력 부분 필요 ***
+
+	// 주의사항 출력 (모든 경우에 정수만 입력할 것)
 	printf("please use integer only\n");
-	printf("Length of Sequence (1~20) : ");
-	scanf_s("%d", &LoS);
-	while (1) // limit Length of Sequence (1~20)
+
+	// 미리 선언할 변수들
+	int cntN, NoS; // for number of sequence
+	
+	// 총 입력 받을 배열의 개수 얻기
+	printf("Number of Sequence (1~10) : ");
+	scanf_s("%d", &NoS);
+	while (1) // 배열의 개수(NoS)를 최대 10개로 제한
 	{
-		if (LoS < 1 || LoS > 20)
+		if (NoS < 1 || NoS > 10)
 		{
-			printf("%d is wrong input\n", LoS);
-			printf("Length of Sequence (1~20) : ");
-			scanf_s("%d", &LoS);
+			printf("%d is wrong input\n", NoS);
+			printf("Number of Sequence (1~10) : ");
+			scanf_s("%d", &NoS);
 		}
 		else
 		{
-			printf("input length: %d\n", LoS); break;
+			printf("input num of seq : %d\n", NoS);
+			break;
 		}
 	}
-	insq = (int*)malloc(sizeof(int) * LoS);
+
+	// LoS 배열 생성 (크기 = NoS)
+	int* LoS = (int*)malloc(sizeof(int) * NoS);
+
+	// NoS번째 배열의 길이 및 값 입력 받기
+	for (cntN = 0;cntN < NoS;cntN++)
+	{
+		printf("Length of Sequence(%d) (1~20) : ", cntN+1);
+		scanf_s("%d", &LoS[cntN]);
+		while (1) // 배열의 길이(LoS)를 최대 20개로 제한
+		{
+			if (LoS[cntN] < 1 || LoS[cntN] > 20)
+			{
+				printf("%d is wrong input\n", LoS[cntN]);
+				printf("Length of Sequence(%d) (1~20) : ", cntN + 1);
+				scanf_s("%d", &LoS[cntN]);
+			}
+			else
+			{
+				printf("input len of seq(%d) : %d\n", cntN+1, LoS[cntN]);
+				break;
+			}
+		}
+	}
+
+	// 주의사항 출력 (숫자와 숫자는 띄어쓰기로 구별할 것)
 	printf("form: num1 num2 num3 ...\n");
-	printf("Sequence: ");
-	for (int i = 0;i < LoS;i++)    scanf_s("%d", &insq[i]);
-	printf("input sequence: ");
-	for (int i = 0;i < LoS;i++)    printf("%d ", insq[i]);
-	printf("\n");
+
+	// 배열 내용 입력 받기
+	int insq[10][20]; // maxNoS=10, maxLoS=20
+	int cntL; // for counting LoS
+	for (cntN = 0;cntN < NoS;cntN++)
+	{
+		printf("Sequence(%d) : ", cntN+1);
+		for (cntL = 0;cntL < LoS;cntL++)
+		{
+			scanf_s("%d", &insq[cntN][cntL]);
+		}
+		printf("input seq(%d) : ", cntN+1);
+		for (cntL = 0;cntL < LoS;cntL++)
+		{
+			printf("%d ", insq[cntN][cntL]);
+		}
+		printf("\n");
+	}
+
+	// *** 여기부터 ***
 	int maxsum = MaxSubsequenceSum(insq, LoS);
 	printf("MAXSUM: %d\n", maxsum);
 	int* subseq = MaxSubSeqSum(insq, LoS, maxsum);
 	printf("SUBSEQ: ");
-	int subseqlen = *subseq;
+	int subseqlen = *subseq; // because subseq[0]=subseqlen
 	for (int i = 0;i < subseqlen;i++)
 	{
 		printf("%d ", subseq[i + 1]);
 	}
 	printf("\n");
-	// subseq의 크기 계산해서 subseq를 for문으로 출력
 	free(insq); free(subseq);
 	return 0;
 }
